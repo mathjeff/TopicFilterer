49edd1bfedef2e8e7167748d38137c7ff89162a7
Jeffry Gaston <gastoj3@gmail.com>
2021-07-19 16:18:12 +0100

    button padding closer to 0 on UWP

---
+++ TopicFilterer/TopicFilterer.UWP/Source/ButtonEffect.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+using Xamarin.Forms.Platform.UWP;
+
+[assembly: ResolutionGroupName("VisiPlacement")]
+[assembly: ExportEffect(typeof(TopicFilterer.UWP.ButtonEffect), "ButtonEffect")]
+namespace TopicFilterer.UWP
+{
+    class ButtonEffect : PlatformEffect
+    {
+        protected override void OnAttached()
+        {
+            Button button = this.Element as Button;
+            button.BorderWidth = 0;
+            // We want padding of 0, but setting padding of 0 doesn't work (perhaps something overrides it later)
+            // If we set a padding of 1, though, that seems to work better, at least
+            button.Padding = 1;
+            button.Margin = 0;
+            //System.Diagnostics.Debug.WriteLine("Attaching " + this + " to element " + this.Element + " and control " + this.Control);
+        }
+        protected override void OnDetached()
+        {
+
+        }
+    }
+}
